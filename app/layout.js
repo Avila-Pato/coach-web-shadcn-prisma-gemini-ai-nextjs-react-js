2bd3104c93497091e998625379f570b0899acc5f
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-08-07 17:18:20 -0400

    starting project installing shadcn and setting up into the projet

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+          <main className="min-h-screen">
+              {children}
+            </main>
+            <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center ">
+              Made by <a href="https://github.com/Avila-Pato">Avila-Pato</a>
+            </div>
+            </footer>
+
+          </ThemeProvider>
       </body>
     </html>
   );
